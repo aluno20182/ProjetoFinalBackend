4b4c4c93929199903b9146f987e731dddd380d99
Ricardo Valente Pinheiro <47854257+aluno20182@users.noreply.github.com>
2020-08-09 17:31:17 +0100

    Criação das rotas de DB Tables

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 //Load express module with `require` directive
 const express = require('express');
 const app = express();
-//const mongoose = require("mongoose");
+
 const mysql = require('mysql');
 const userRoute = require('./Routes/User');
 var bodyParser = require('body-parser')
-const port = 3306;
 
-/* // Connect to MongoDB
-mongoose
-  .connect(
-    "mongodb+srv://Ricardo_VP:projetofinal@projetofinal-j6wen.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
- */
+const port = 3000;
+
 
 //Criar a conecção
 const db = mysql.createConnection({
@@ -25,27 +17,63 @@ const db = mysql.createConnection({
   database : 'nodesql'
 });
 
+
+//###########################################################################     DATABASE      ################################################################################## 
+
 //Coneção DB
-db.connect(() => {
-  let sql = 'CREATE DATABASE nodesql'
+db.connect((err) => {
   if(err){
-    throw err;
+    console.log(err);
   }
   console.log('MySQL connected')
 });
 
 
-//Create Table
-app.get('/createuserstable', (req, res) => {
-  let sql = 'CREATE TABLE users'
+//Create DB
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE nodesql';
+  db.query(sql, (err, result) => {
+    if(err){
+      console.log( err);
+    } 
+    console.log(result);
+    res.send('Database created...')
+  });
+});
 
+//###########################################################################     TABLES      ################################################################################## 
+
+//Create Table Users
+app.get('/createuserstable', (req, res) => {
+  let sql = 'CREATE TABLE users (user_id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) UNIQUE, password VARCHAR(255), user_name VARCHAR(255) NOT NULL UNIQUE, points INT NOT NULL)';
+  db.query(sql, (err, result) => {
+    if(err){
+      console.log( err);
+    };
+    console.log(result);
+    res.send('Users table created...')
+  })
 })
+
+//Create Table Tokens
+app.get('/createtokenstable', (req, res) => {
+  let sql = 'CREATE TABLE tokens (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT UNIQUE, token VARCHAR(255))';
+  db.query(sql, (err, result) => {
+    if(err){
+      console.log(err);
+    };
+    console.log(result);
+    res.send('Tokens table created...')
+  })
+})
+
+
 
 app.use(bodyParser.json())
 app.use('/', userRoute);
 
 
-//Launch listening server on port 3306
+//Launch listening server on port 3000
 app.listen(port, function () {
-  console.log('app listening on port 3306!')
+  console.log('app listening on port 3000!')
 })
