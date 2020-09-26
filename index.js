b89ec78c90cc2f60280afeb8f03579ec09bf153c
Ricardo Valente Pinheiro <47854257+aluno20182@users.noreply.github.com>
2020-09-27 00:11:39 +0100

    new update

--- index.js
+++ index.js
@@ -405,7 +405,7 @@ app.listen(process.env.PORT || 5000, function () {
   });
 
   app.get('/', (req, res) => {
-    fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
+    app.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
         res.send(text);
     });
 });
